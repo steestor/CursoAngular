56ae7d5500371b8d3949b62c2b0fcbd113ca2938
steestor <steestor@outlook.es>
2020-10-13 16:07:24 +0200

    [MASTERGYM] - Añadido de nuevo

--- mastergym/e2e/src/app.e2e-spec.ts
+++ mastergym/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('mastergym app is running!');
+    expect(page.getTitleText()).toEqual('MasterGym app is running!');
   });
 
   afterEach(async () => {
--- mastergym/src/app/app.component.spec.ts
+++ mastergym/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mastergym'`, () => {
+  it(`should have as title 'MasterGym'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mastergym');
+    expect(app.title).toEqual('MasterGym');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('mastergym app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('MasterGym app is running!');
   });
 });
