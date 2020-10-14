baf07e0040a03cc0b02ce75a14364e27b064107e
steestor <steestor@outlook.es>
2020-10-14 12:22:57 +0200

    [MASTERGYM] - Errores específicos

--- MasterGym/src/app/login/login.component.ts
+++ MasterGym/src/app/login/login.component.ts
@@ -1,28 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AngularFireAuth } from 'angularfire2/auth';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { TranslateService } from "@ngx-translate/core";
+import { AngularFireAuth } from "angularfire2/auth";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
   formularioLogin: FormGroup;
   datosCorrectos: boolean = true;
   campoNoValido: boolean = true;
-  mensajeError: string = '';
+  mensajeError: string = "";
+  mensajeError1: string = "";
+  mensajeError2: string = "";
+  public activeLang = "es";
 
   constructor(
     private formBuilder: FormBuilder,
-    public afAuth: AngularFireAuth
+    public afAuth: AngularFireAuth,
+    public translate: TranslateService
   ) {}
 
   ngOnInit(): void {
     this.formularioLogin = this.formBuilder.group({
-      email: ['', Validators.compose([Validators.required, Validators.email])],
-      password: ['', Validators.compose([Validators.required])],
+      email: ["", Validators.compose([Validators.required, Validators.email])],
+      password: ["", Validators.compose([Validators.required])],
     });
+
+    this.translate.setDefaultLang(this.activeLang);
   }
 
   ingresar() {
@@ -35,17 +42,26 @@ export class LoginComponent implements OnInit {
           this.formularioLogin.value.password
         )
         .then((usuario) => {
-          console.log(usuario);
+          // console.log(usuario);
         })
         .catch((e) => {
           this.datosCorrectos = false;
-          this.mensajeError = e.message;
-          console.log(this.mensajeError);
+          if (e.code === "auth/user-not-found") {
+            this.mensajeError1 = e.message;
+            this.mensajeError2 = "";
+            this.mensajeError = "";
+          } else {
+            this.mensajeError2 = e.message;
+            this.mensajeError1 = "";
+            this.mensajeError = "";
+          }
         });
     } else {
       this.datosCorrectos = false;
       this.mensajeError =
-        'Se ha producido un error. Revisa que los datos sean correctos';
+        "Se ha producido un error. Revisa que los datos sean correctos";
+      this.mensajeError1 = "";
+      this.mensajeError2 = "";
     }
   }
 }
--- mastergym/src/app/app.module.ts
+++ mastergym/src/app/app.module.ts
@@ -10,13 +10,29 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
+//Translation
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslationComponent } from './translation/translation.component';
+
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, TranslationComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     AngularFireModule.initializeApp(environment.firebase),
     ReactiveFormsModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (http: HttpClient) => {
+          return new TranslateHttpLoader(http);
+        },
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [AngularFireAuth],
   bootstrap: [AppComponent],
